9d83abcfc4d48384fc04444d68fbfa2e5ce63aae
mergakitlla <126342519+mergakitlla@users.noreply.github.com>
2023-03-25 18:31:42 +0300

    Update Budget.js

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,11 +1,31 @@
-import React, { useContext } from 'react';
+import React, {useState, useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 const Budget = () => {
     const { budget } = useContext(AppContext);
-    return (
+    const [budget2, setBudget] = useState({budget});
+        return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+           
+           Budget: £
+
+            <input
+                required='required'
+                type='number'
+                id='Budget'
+                value={budget}
+                step={10}
+                min = {50}
+                max = {20500}
+                style={{ marginLeft: '2rem', size: 10 }}
+                onChange={(event) => setBudget(event.target.value)}
+               // onChange={(event) => handlecheckValue(event.target.value)}
+                > 
+            </input>
+
+
         </div>
     );
 };
+// onChange={(event) => setBudget(event.target.value)}
+          //<span>Budget: £{budget}</span> 
 export default Budget;
